7f8681624d759f010ac5efc817f48e6ab3f63606
Alondra Gamez <agamez@appdev.yearup.org>
2025-04-28 13:26:38 -0500

    added psuedocode to give me an outline

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,8 +1,39 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
 
+    //scanner allows user input
+    static Scanner ledgerScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        //home screen (runs until user chooses to exit the application)
+        //D) Add Deposit: prompt user for the debit information and save it in the csv file
+        //P) Make Payment (Debit): prompt user for the debit information and to save it in the csv file
+        //L) Ledger: display the ledger screen
+        //X) Exit: exit the application
+//        System.out.print("");
+//        String userSelection = ledgerScanner.nextLine();
+
+        //ledger screen (all entries should show the newest entries first)
+        //A) All: display all entries
+        //D) Deposits: display only the entries that are deposits into the account
+        //P) Payments: display only the negative entries/payments
+        //R) Reports: a new screen that allows the user to run pre-defined reports or to run a custom search
+        //-1) Month To Date
+        //-2) Previous Month
+        //-3) Year To Date
+        //-4) Previous Year
+        //-5) Search By Vendor: prompt user for the vendor name and display all entries for that vendor
+        //-0) Back: go back to Reports page
+        //H) Home: go back to home screen
+
+
+
+
+
 
     }
 }
